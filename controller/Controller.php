d8ab46219d5c6bd40f08ec1d7fe0c0a0729f7903
nicolasalemano <nico.alemano@gmail.com>
2017-11-07 11:26:06 -0300

    modificacion controller agregado de puerto

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,9 +1,10 @@
 <?php
-define('HOME', 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/HOME');
-define('EQUIPO', 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/equipo');
-define('JUGADORES', 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/jugadores');
-define('LOGIN', 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/login');
-define('LOGOUT', 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/logout');
+define('HOME', 'http://'.$_SERVER['SERVER_NAME']  .':'. $_SERVER['SERVER_PORT']. dirname($_SERVER['PHP_SELF']).'/HOME');
+define('EQUIPO', 'http://'.$_SERVER['SERVER_NAME']  . ':'.$_SERVER['SERVER_PORT']. dirname($_SERVER['PHP_SELF']).'/equipo');
+define('JUGADORES', 'http://'.$_SERVER['SERVER_NAME']  . ':'.$_SERVER['SERVER_PORT']. dirname($_SERVER['PHP_SELF']).'/jugadores');
+define('LOGIN', 'http://'.$_SERVER['SERVER_NAME']  . ':'.$_SERVER['SERVER_PORT']. dirname($_SERVER['PHP_SELF']).'/login');
+define('LOGOUT', 'http://'.$_SERVER['SERVER_NAME'] . ':'.$_SERVER['SERVER_PORT']. dirname($_SERVER['PHP_SELF']).'/logout');
+define('ADMIN', 'http://'.$_SERVER['SERVER_NAME'] . ':'.$_SERVER['SERVER_PORT']. dirname($_SERVER['PHP_SELF']).'/adminUser');
 
 
 class Controller
@@ -13,14 +14,12 @@ class Controller
   protected $model;
   protected $baseURL;
 
-    function __construct(){
-        $this->baseURL = 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/';
+    function __construct()
+    {
+        $this->baseURL = 'http://'.$_SERVER['SERVER_NAME'] . ':'.$_SERVER['SERVER_PORT']. dirname($_SERVER['PHP_SELF']).'/';
     }
 
-    public function goBack($valor = ''){
 
-        header("Location : ".baseURL.$valor);
-}
 
 }
 ?>
