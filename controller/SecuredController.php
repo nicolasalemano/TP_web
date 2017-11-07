5017ecb52e97f6331b8fb4b7f2d486949bc34722
nicolasalemano <nico.alemano@gmail.com>
2017-11-07 11:33:00 -0300

    Modificacion en secureController

--- controller/SecuredController.php
+++ controller/SecuredController.php
@@ -6,31 +6,42 @@ class SecuredController extends Controller
   function __construct()
   {
     session_start();
-    if(isset($_SESSION['USER'])){
+    if(isset($_SESSION['USER']))
+    {
       if (time() - $_SESSION['LAST_ACTIVITY'] > 25600) {
         header('Location: '.LOGOUT);
         die();
       }
       $_SESSION['LAST_ACTIVITY'] = time();
     }
-    else {
-      //header('Location: '.LOGIN);
-      //die();
-    }
+
   }
-  public function getUser(){
-      if(isset($_SESSION['USER'])) {
-        return $_SESSION['USER'];
+  public function getUser()
+  {
+      if(isset($_SESSION['USER']))
+      {
+          return $_SESSION['USER'];
       }else{
         return false;
       }
   }
 
-    public function verify(){
+
+    public function verify()
+    {
         if(!isset($_SESSION['USER'])) {
             header('Location: ' . LOGIN);
             die();
         }
+    }
+    public function verificaPermiso()
+    {
+        if(isset($_SESSION['USER']) && $_SESSION['permissions']==1)
+        {
+            return 1;
+        }
+        else
+            return 0;
     }
 }
 
